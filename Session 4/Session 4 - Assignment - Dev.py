07dbbd4dedeaa70a353a8067bbc028e13112c22f
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-05-24 18:26:33 +0530

    assignment 4 cleaning up for attempt 1

--- Session 4/Session 4 - Assignment - Dev.py
+++ Session 4/Session 4 - Assignment - Dev.py
@@ -111,28 +111,28 @@ def train(model, device, train_loader, optimizer):
 	train_losses.append(train_loss/len(train_loader))
 	
 def test(model, device, test_loader):
-		model.eval()
-	
-		test_loss = 0
-		correct = 0
-	
-		with torch.no_grad():
-			for batch_idx, (data, target) in enumerate(test_loader):
-				data, target = data.to(device), target.to(device)
-			
-				output = model(data)
-				test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-			
-				correct += GetCorrectPredCount(output, target)
-					
-					
-		test_loss /= len(test_loader.dataset)
-		test_acc.append(100. * correct / len(test_loader.dataset))
-		test_losses.append(test_loss)
-	
-		print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-				test_loss, correct, len(test_loader.dataset),
-				100. * correct / len(test_loader.dataset)))
+	model.eval()
+
+	test_loss = 0
+	correct = 0
+
+	with torch.no_grad():
+		for batch_idx, (data, target) in enumerate(test_loader):
+			data, target = data.to(device), target.to(device)
+		
+			output = model(data)
+			test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+		
+			correct += GetCorrectPredCount(output, target)
+				
+				
+	test_loss /= len(test_loader.dataset)
+	test_acc.append(100. * correct / len(test_loader.dataset))
+	test_losses.append(test_loss)
+
+	print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+			test_loss, correct, len(test_loader.dataset),
+			100. * correct / len(test_loader.dataset)))
 
 #%%
 model = Net().to(device)
