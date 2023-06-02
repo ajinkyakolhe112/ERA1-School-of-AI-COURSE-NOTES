48a2696bdcd92d42dd59162feee8d0f1ae6cb392
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-02 18:26:36 +0530

    week 5 experimentation

---
+++ Session 5 - Pytorch 101/experiments/pytorch experimentation.py
@@ -0,0 +1,7 @@
+for param in model.parameters():
+	"grad is on param level not individual element level. grad is an tensor of same shape as params"
+	print(param.shape,param.grad.shape)
+	if len(param.shape)>1:
+		print("%0.2f"%param.grad)
+		for kernelIndex in range(param.shape[0]):
+			print(param[kernelIndex])
