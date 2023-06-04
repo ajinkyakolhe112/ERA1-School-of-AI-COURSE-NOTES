14b561d96e0d88b8229a59e4b3b687c77102a10d
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-04 14:13:40 +0530

    increasing RF & adjusting for output size matrix multiplication

--- Session 6 - Backprop & Architectures/attempt 1/model.py
+++ Session 6 - Backprop & Architectures/attempt 1/model.py
@@ -9,18 +9,24 @@ class baselineModel(nn.Module):
 		"4 Blocks of Feature Extraction with increasing features."
 		"RF = 9*9. Image = 28*28. Goal RF = 28*28. Delta RF for kernel = 3-1. Goal Depth = 28/2 = 14"
 		"RF = Image Size - Output Size (Easier RF Calculation)"
-		nn.Conv2d(1,32,(3,3))  # RF = 3*3
-		nn.Conv2d(32,32,(3,3)) # RF = 5*5
+		nn.Conv2d(1,32,(3,3),padding=1)  # RF = 3*3
+		nn.Conv2d(32,32,(3,3),padding=1) # RF = 5*5
 		nn.MaxPool2d(2,2)      # RF = 10*10
+		# output = 14*14
 		
-		nn.Conv2d(32,64,(3,3)) # RF = 12*12
-		nn.Conv2d(64,64,(3,3)) # RF = 14*14
-		nn.MaxPool2d(2,2)	   # RF = 28*28. GOAL RF Reached
+		nn.Conv2d(32,64,(3,3),padding=1) # RF = 12*12
+		nn.Conv2d(64,64,(3,3),padding=1) # RF = 14*14
+		nn.MaxPool2d(2,2)	   # RF = 28*28. GOAL RF Reached. 
+		# output = 7*7
+		
+		# Without padding output size = 1*1. Next 2 blocks need 2d data for convolution to extract features.
 
 		nn.Conv2d(64,128,(3,3))
+		# output = 5*5
 		
-		nn.Conv2d(32,32,(3,3))
-		
+		nn.Conv2d(128,256,(3,3))
+		# output = 3*3
+
 		
 	def forward(self, dataX):
 		pass
