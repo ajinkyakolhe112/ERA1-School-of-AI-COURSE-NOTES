wrong 1*1 conv for flatten, and normalizing data mistake

Session 6 - Backprop & Architectures/attempt 3/src/data.py
@@ -6,11 +6,17 @@ import logging
 train_dataset = datasets.MNIST(".data",
                                train=True,
                                download=True,
-                               transform=transforms.ToTensor())
+                               transform=transforms.Compose([
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.1307, ), (0.3081, )),
+                               ]))
 test_dataset = datasets.MNIST(".data",
                               train=False,
                               download=True,
-                              transform=transforms.ToTensor())
+                              transform=transforms.Compose([
+                                  transforms.ToTensor(),
+                                  transforms.Normalize((0.1307, ), (0.3081, )),
+                              ]))
 
 train_dataloader = torch.utils.data.DataLoader(train_dataset,
                                                batch_size=64,
@@ -19,15 +25,17 @@ test_dataloader = torch.utils.data.DataLoader(test_dataset,
                                               batch_size=64,
                                               shuffle=True)
 
-def test():
 
-    for batch_id,(x_train_batch,y_train_batch) in enumerate(train_dataloader):
-        X,Y = x_train_batch, y_train_batch
-        print("Batch = ",batch_id,"\t with X Shape = ",X.shape)
+def test_data():
 
+    for batch_id, (x_train_batch,
+                   y_train_batch) in enumerate(train_dataloader):
+        X, Y = x_train_batch, y_train_batch
+        print("Batch = ", batch_id, "\t with X Shape = ", X.shape)
 
         if batch_id == 5:
             break
 
-if __name__ =="__main__":
-    test()
+
+if __name__ == "__main__":
+    test_data()

Session 6 - Backprop & Architectures/attempt 3/src/model.py
@@ -10,14 +10,14 @@ class model_v1(nn.Module):
             "relu": nn.ReLU(), 
             "conv2": nn.Conv2d(32,64,(3,3),1,1),    # Delta RF = +2
             "relu": nn.ReLU(),
-            # kernel = [ kernel = (3,3), stride , padding ]
+                                                    # kernel = [ kernel = (3,3), stride , padding ]
             "pool1": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2
 
             "conv3": nn.Conv2d(64,128,(3,3),1,1),   # Delta RF = +2
             "relu": nn.ReLU(),
             "conv4": nn.Conv2d(128,256,(3,3),1,1),  # Delta RF = +2
             "relu": nn.ReLU(),
-            # kernel = [ kernel = (3,3), stride , padding ]
+                                                    # kernel = [ kernel = (3,3), stride , padding ]
             "pool2": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2. RF Total = 28
             
             "conv5": nn.Conv2d(256,512,(3,3),1,0),  # Delta RF = +2
@@ -44,6 +44,7 @@ class model_v1(nn.Module):
         Y_pred = local_model(X)
         Y_pred = self.sequential_model(x_train_batch)
         return Y_pred
+
 
 def test_model():
     img = torch.randn(1,28,28)

Session 6 - Backprop & Architectures/attempt 3/src/utils.py
@@ -5,6 +5,46 @@ import data
 import model as models
 import torch.nn.functional as F
 
+class reshape(nn.Module):
+    def __init__(self,pixels):
+        super().__init__()
+        self.pixels = pixels
+    def forward(self,x):
+        x = x.view(-1,512*9)
+        return x
+reshaper = reshape(512*9)
+test_model_outside = nn.Sequential(*{                          # RF Start = 1
+            # img = [ in_channels, out_channels ] , kernel = [ kernel = (3,3), stride , padding ]
+            "conv1": nn.Conv2d(1,32,(3,3),1,1),     # Delta RF = +2
+            "relu": nn.ReLU(), 
+            "conv2": nn.Conv2d(32,64,(3,3),1,1),    # Delta RF = +2
+            "relu": nn.ReLU(),
+                                                    # kernel = [ kernel = (3,3), stride , padding ]
+            "pool1": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2
+
+            "conv3": nn.Conv2d(64,128,(3,3),1,1),   # Delta RF = +2
+            "relu": nn.ReLU(),
+            "conv4": nn.Conv2d(128,256,(3,3),1,1),  # Delta RF = +2
+            "relu": nn.ReLU(),
+                                                    # kernel = [ kernel = (3,3), stride , padding ]
+            "pool2": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2. RF Total = 28
+            
+            "conv5": nn.Conv2d(256,512,(3,3),1,0),  # Delta RF = +2
+            "relu": nn.ReLU(),
+            "conv6": nn.Conv2d(512,512,(3,3),1,0),  # Delta RF = +2, Output = (K:512,H:3,W:3)
+            "relu": nn.ReLU(),
+            "reshape": reshaper,
+
+            # "merge": nn.Conv2d(512,1,(1,1),1,0),    # Reducing Channels to 1. So that we get [1,3*3]. Shape for Linear
+            # "flatten": nn.Flatten(),                # RF Total = 32, Output = (512,9)
+            
+            "fc1": nn.Linear(512*9,50),               # Output = 512,10
+            "relu": nn.ReLU(),
+            "fc2": nn.Linear(50,10),                # 10 Neurons for 10 Classes. Output = [1*10]
+            "softmax": nn.LogSoftmax(dim=1),           # dim0 = 1, dim1= 10 in Output = (1,10)
+            # Using softmax to check if probability sum == 1
+
+        }.values())
 "X , f(x,W), ErrorCalculator, Recursive ErrorGradient, Update, Device"
 def trainBatch(X,Y,model,ErrorCalculator,optimizer,device):
     Y_pred = model(X),"builds forward graph of f(X,W)"
@@ -57,7 +97,9 @@ def test_training():
     
     lr = 0.01
     optimizer = torch.optim.SGD(model.parameters(), lr)
-    train(train_dataloader,model,ErrorCalculator,optimizer)
+
+    "custom test model injected here"
+    train(train_dataloader,test_model_outside,ErrorCalculator,optimizer)
 
 
 if __name__=="__main__":