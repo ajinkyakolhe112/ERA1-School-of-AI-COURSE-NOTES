ad1866b64b850787c7210f3fb83e7df70524078c
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-06 17:35:59 +0530

    model for conv image

--- Session 6 - Backprop & Architectures/attempt 3/src/model.py
+++ Session 6 - Backprop & Architectures/attempt 3/src/model.py
@@ -4,31 +4,37 @@ import torch
 class model_v1(nn.Module):
     def __init__(self):
         super().__init__()
-        self.container = {      # RF Start = 1
-            1: nn.Conv2d,       # Delta RF = +2 
-            2: nn.Conv2d,       # Delta RF = +2
-            3: nn.MaxPool2d,    # Delta RF = *2
-            4: nn.Conv2d,       # Delta RF = +2
-            5: nn.Conv2d,       # Delta RF = +2
-            6: nn.MaxPool2d,    # Delta RF = *2. RF Total = 28
-            7: nn.Conv2d,       # Delta RF = +2
-            8: nn.Conv2d,       # Delta RF = +2
-            9: nn.Flatten,      # RF Total = 32
-
-            10: nn.Linear,
-
-            11: nn.Linear,
-            12: nn.Softmax,
+        self.container = {                      # RF Start = 1
+            "conv1": nn.Conv2d(1,32,3,1,1),     # Delta RF = +2 
+            "conv2": nn.Conv2d(32,64,3,1,1),    # Delta RF = +2
+            "pool1": nn.MaxPool2d(2,2,0),       # Delta RF = *2
+            "conv3": nn.Conv2d(64,128,3,1,1),   # Delta RF = +2
+            "conv4": nn.Conv2d(128,256,3,1,1),  # Delta RF = +2
+            "pool2": nn.MaxPool2d(2,2,0),       # Delta RF = *2. RF Total = 28
+            "conv5": nn.Conv2d(256,512,3,1,0),  # Delta RF = +2
+            "conv6": nn.Conv2d(512,512,3,1,0),  # Delta RF = +2, Output = (K:512,H:3,W:3)
 
         }
+        self.sequential_model = nn.Sequential( *self.container.values() )
 
     def forward(self,x_train_batch):
         X = x_train_batch
-        return self.model(X)
+        model = self.sequential_model
+
+        result = model(X)
+        result = self.sequential_model(x_train_batch)
+        return result
 
 def test_model():
+    img = torch.randn(1,28,28)
     model = model_v1()
-    model(torch.randn(1,28,28))
+
+    output = model.sequential_model(img)
+    output = model(img)
+    "This structure allows for increamental testing layer by layer"
+    output.shape
 
 if __name__=="__main__":
     test_model()
+
+    print("END")
