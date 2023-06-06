31aa73114e99c6b7f0be97cb46dcaef83dc7b3d5
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-06 15:03:09 +0530

    basic sequential model architecture done

--- Session 6 - Backprop & Architectures/attempt 3/src/model.py
+++ Session 6 - Backprop & Architectures/attempt 3/src/model.py
@@ -4,13 +4,27 @@ import torch
 class model_v1(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = nn.Sequential()
+        self.container = {      # RF Start = 1
+            1: nn.Conv2d,       # Delta RF = +2 
+            2: nn.Conv2d,       # Delta RF = +2
+            3: nn.MaxPool2d,    # Delta RF = *2
+            4: nn.Conv2d,       # Delta RF = +2
+            5: nn.Conv2d,       # Delta RF = +2
+            6: nn.MaxPool2d,    # Delta RF = *2. RF Total = 28
+            7: nn.Conv2d,       # Delta RF = +2
+            8: nn.Conv2d,       # Delta RF = +2
+            9: nn.Flatten,      # RF Total = 32
+
+            10: nn.Linear,
+
+            11: nn.Linear,
+            12: nn.Softmax,
+
+        }
 
     def forward(self,x_train_batch):
         X = x_train_batch
         return self.model(X)
-        result = 0
-        return result
 
 def test_model():
     model = model_v1()
