42696baa5d161f34a0cfc4642bc58c29b0d0a2ec
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-07 14:32:10 +0530

    simpifying for running experiments. simplified utils & model

--- Session 6 - Backprop & Architectures/attempt 3/src/model.py
+++ Session 6 - Backprop & Architectures/attempt 3/src/model.py
@@ -1,6 +1,14 @@
 from torch import nn as nn
 import torch
 
+class reshape(nn.Module):
+    def __init__(self,pixels):
+        super().__init__()
+        self.pixels = pixels
+    def forward(self,x):
+        x = x.view(-1,512*9)
+        return x
+
 class model_v1(nn.Module):
     def __init__(self):
         super().__init__()
@@ -22,13 +30,13 @@ class model_v1(nn.Module):
             
             "conv5": nn.Conv2d(256,512,(3,3),1,0),  # Delta RF = +2
             "relu": nn.ReLU(),
-            "conv6": nn.Conv2d(512,512,(3,3),1,0),  # Delta RF = +2, Output = (K:512,H:3,W:3)
+            "conv6": nn.Conv2d(512,1024,(3,3),1,0),  # Delta RF = +2, Output = (K:512,H:3,W:3)
             "relu": nn.ReLU(),
 
-            "merge": nn.Conv2d(512,1,(1,1),1,0),    # Reducing Channels to 1. So that we get [1,3*3]. Shape for Linear
-            "flatten": nn.Flatten(),                # RF Total = 32, Output = (512,9)
-            
-            "fc1": nn.Linear(3*3,50),               # Output = 512,10
+            # "merge": nn.Conv2d(512,1,(1,1),1,0),    # Reducing Channels to 1. So that we get [1,3*3]. Shape for Linear
+            # "flatten": nn.Flatten(),                # RF Total = 32, Output = (512,9)
+            "reshaper": reshape(1024*9),
+            "fc1": nn.Linear(1024*3*3,50),               # Output = 512,10
             "relu": nn.ReLU(),
             "fc2": nn.Linear(50,10),                # 10 Neurons for 10 Classes. Output = [1*10]
             "softmax": nn.LogSoftmax(dim=1),           # dim0 = 1, dim1= 10 in Output = (1,10)
@@ -39,15 +47,15 @@ class model_v1(nn.Module):
 
     def forward(self,x_train_batch):
         X = x_train_batch
-        local_model = self.sequential_model
+        model = self.sequential_model
 
-        Y_pred = local_model(X)
+        Y_pred = model(X)   # Instead of writing forward, directly calling forward of sequential
         Y_pred = self.sequential_model(x_train_batch)
         return Y_pred
 
 
 def test_model():
-    img = torch.randn(1,28,28)
+    img = torch.randn(7,1,28,28)
     model = model_v1()
 
     y_pred = model.sequential_model(img)
--- Session 6 - Backprop & Architectures/attempt 3/src/utils.py
+++ Session 6 - Backprop & Architectures/attempt 3/src/utils.py
@@ -1,60 +1,27 @@
 import torch
 from tqdm import tqdm
 import torch.nn as nn
-import data
-import model as models
+from data import *
+from model import *
 import torch.nn.functional as F
 
-class reshape(nn.Module):
-    def __init__(self,pixels):
-        super().__init__()
-        self.pixels = pixels
-    def forward(self,x):
-        x = x.view(-1,512*9)
-        return x
-reshaper = reshape(512*9)
-test_model_outside = nn.Sequential(*{                          # RF Start = 1
-            # img = [ in_channels, out_channels ] , kernel = [ kernel = (3,3), stride , padding ]
-            "conv1": nn.Conv2d(1,32,(3,3),1,1),     # Delta RF = +2
-            "relu": nn.ReLU(), 
-            "conv2": nn.Conv2d(32,64,(3,3),1,1),    # Delta RF = +2
-            "relu": nn.ReLU(),
-                                                    # kernel = [ kernel = (3,3), stride , padding ]
-            "pool1": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2
-
-            "conv3": nn.Conv2d(64,128,(3,3),1,1),   # Delta RF = +2
-            "relu": nn.ReLU(),
-            "conv4": nn.Conv2d(128,256,(3,3),1,1),  # Delta RF = +2
-            "relu": nn.ReLU(),
-                                                    # kernel = [ kernel = (3,3), stride , padding ]
-            "pool2": nn.MaxPool2d((2,2),2,0),       # Delta RF = *2. RF Total = 28
-            
-            "conv5": nn.Conv2d(256,512,(3,3),1,0),  # Delta RF = +2
-            "relu": nn.ReLU(),
-            "conv6": nn.Conv2d(512,512,(3,3),1,0),  # Delta RF = +2, Output = (K:512,H:3,W:3)
-            "relu": nn.ReLU(),
-            "reshape": reshaper,
-
-            # "merge": nn.Conv2d(512,1,(1,1),1,0),    # Reducing Channels to 1. So that we get [1,3*3]. Shape for Linear
-            # "flatten": nn.Flatten(),                # RF Total = 32, Output = (512,9)
-            
-            "fc1": nn.Linear(512*9,50),               # Output = 512,10
-            "relu": nn.ReLU(),
-            "fc2": nn.Linear(50,10),                # 10 Neurons for 10 Classes. Output = [1*10]
-            "softmax": nn.LogSoftmax(dim=1),           # dim0 = 1, dim1= 10 in Output = (1,10)
-            # Using softmax to check if probability sum == 1
-
-        }.values())
 "X , f(x,W), ErrorCalculator, Recursive ErrorGradient, Update, Device"
-def trainBatch(X,Y,model,ErrorCalculator,optimizer,device):
-    Y_pred = model(X),"builds forward graph of f(X,W)"
-    ErrorValue = errorCalculator(Y_pred,Y)
-    ErrorValue.backward(),"Calculate Gradient recursively"
-    optimizer.step(),"Update Each Parameter W according to Gradient"
-    optimizer.zero_grad(),"clear graph for next batch execution"
+def trainBatch(X,Y,model,ErrorCalculator,optimizer,device=None):
+    Y_pred_probs = model(X)                       # builds forward graph of f(X,W)
+    ErrorValue = ErrorCalculator(Y_pred_probs,Y)  # Calculate Error Value
+    ErrorValue.backward()                   # Calculate Gradient recursively
+    # optimizer.step()                      # Update Each Parameter W according to Gradient
+    for name,param in model.named_parameters():
+            delta = param.grad * 0.01
+            param = param - param.grad * 0.01
+            # delta = param.grad * 0.01 . ERROR. Done AFTER param is updated. Hence grad depopulated
+    optimizer.zero_grad()                   # clear graph for next batch execution"
+
+    print("Predicted Value",Y_pred_probs.argmax(dim=1),"Actual Value",Y,"Single Error Value",ErrorValue.item())
+    # print("Predicted Value",Y_pred_probs.argmax(dim=1),"Predicted Probs",Y_pred_probs)
 
 "X , f(x,W), ErrorCalculator, Recursive ErrorGradient, Update, Device"
-def train(train_dataloader,model,errorCalculator,optimizer,device=None):
+def trainEpoch(train_dataloader,model,errorCalculator,optimizer,device=None):
     model.train(mode=True)
     trainingProgress = tqdm(train_dataloader)
     
@@ -79,27 +46,38 @@ def train(train_dataloader,model,errorCalculator,optimizer,device=None):
         "Update W of each neuron recursively"
         # optimizer.step()
         for param in model.parameters():
-            delta = param.grad * 0.01
+            non_zero_grads = param.grad.count_nonzero()
             param = param - param.grad * 0.01
             # delta = param.grad * 0.01 . ERROR. Done AFTER param is updated. Hence grad depopulated
 
         "batch details"
         trainingProgress.set_description_str("Correctly Predicted %d,\t Error Value %0.4f"%(correct_preds,errorValue.item()))
-        print(Y_pred_single,Y_batch)
+        # print(Y_pred_single,Y_batch)
         optimizer.zero_grad()
+
+def trainEpochs(train_dataloader,model,errorCalculator,optimizer,epochs=10,device=None):
+    for epoch_no in range(epochs):
+        trainEpoch(train_dataloader,model,errorCalculator,optimizer)
 
 "X , f(x,W), ErrorCalculator, Update, Device"
 def test_training():
 
-    train_dataloader, test_dataloader = data.train_dataloader,data.test_dataloader
-    model = models.model_v1().sequential_model
+    train_dataloader, test_dataloader
+    model = model_v1().sequential_model
     ErrorCalculator = torch.nn.functional.nll_loss
     
     lr = 0.01
     optimizer = torch.optim.SGD(model.parameters(), lr)
 
-    "custom test model injected here"
-    train(train_dataloader,test_model_outside,ErrorCalculator,optimizer)
+    batch_no = 0
+    epochs = 1
+    while epochs < 20:
+        iterator = iter(train_dataloader)
+        while batch_no < 50:
+            X,Y = next(iterator)
+            trainBatch(X,Y,model,ErrorCalculator,optimizer)
+            batch_no += 1
+        epochs += 1
 
 
 if __name__=="__main__":
