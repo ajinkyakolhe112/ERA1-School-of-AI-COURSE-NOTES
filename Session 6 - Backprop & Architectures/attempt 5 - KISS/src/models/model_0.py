db5f6b05b798f687c6215e3641cd28850eed6361
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2023-06-09 13:30:35 +0530

    model 2 original copies

--- Session 6 - Backprop & Architectures/attempt 5 - KISS/src/models/model_0.py
+++ Session 6 - Backprop & Architectures/attempt 5 - KISS/src/models/model_0.py
@@ -14,37 +14,28 @@ class reshape(nn.Module):
 container = {
     # ! RF Start = 1
     # Format: img = [ in_channels, out_channels ] , kernel = [ size, stride, padding ]
-	# BLOCK 1
     "conv1": nn.Conv2d(1, 32, (3, 3), 1, 1),
     "relu": nn.ReLU(),
-
     "conv2": nn.Conv2d(32, 64, (3, 3), 1, 1),
     "relu": nn.ReLU(),
-
     "pool1": nn.MaxPool2d((2, 2), 2, 0),
 
-	# BLOCK 2
     "conv3": nn.Conv2d(64, 128, (3, 3), 1, 1),
     "relu": nn.ReLU(),
-
     "conv4": nn.Conv2d(128, 256, (3, 3), 1, 1),
     "relu": nn.ReLU(),
-
     "pool2": nn.MaxPool2d((2, 2), 2, 0),
 
-	# BLOCK 3
     "conv5": nn.Conv2d(256, 512, (3, 3), 1, 0),
     "relu": nn.ReLU(),
     
-	# BLOCK 4
     "conv6": nn.Conv2d(512, 1024, (3, 3), 1, 0),    # ! Output = (K:1024, H:3, W:3)
     "relu": nn.ReLU(),
-    # ! Target RF Reached: Have seen entire image now
-	# ! BLOCK 4: OBJECT or CLASSIFICATION OBJECT for each pixel
+    # ! Target RF Reached: All Features Extracted By Now
 
 	# "conv7": nn.Conv2d(32, 10, 3),
 
-    "reshaper": reshape(1024*3*3),	# One Pixel for One Object from BLOCK 4
+    "reshaper": reshape(1024*3*3),
     "fc1": nn.Linear(1024*3*3,50),
 	"fc2": nn.Linear(50,10),                # 10 Neurons for 10 Classes. Output = [1*10]
     "log_softmax": nn.LogSoftmax(dim=1),
